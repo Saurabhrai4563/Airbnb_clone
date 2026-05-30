ed0ff01f8d595b3061b73d78b45927e4bf4dcc6f
Saurabh Shekhar Rai <122366063+Saurabhrai4563@users.noreply.github.com>
2026-05-30 23:12:34 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,38 @@
+const { ref } = require("joi");
+const mongoose = require("mongoose");
+const Review = require("./review.js")
+const Schema = mongoose.Schema;
+
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1772339164169-4391cd6f0096?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => //  default link for imgae using set :
+            v === "" ? "https://images.unsplash.com/photo-1772339164169-4391cd6f0096?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+})
+listingSchema.post("findOneAndDelete", async (listing) => {
+    console.log(listing);
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } })
+    }
+})
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
---
+++ routes/listing.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const wrapAsync = require("../utils/wrapAsync.js")
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+const { isLoggedIn } = require("../middleware.js");
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+router.get("/", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", { allListings });
+    //res.send("working listing");
+})
+//new route
+router.get("/new", isLoggedIn, (req, res) => {
+    //console.log(req.user);
+    res.render("listings/new");
+})
+//create route
+
+router.post("/", isLoggedIn, validateListing,
+    wrapAsync(async (req, res, next) => {
+        let newListing = new Listing(req.body.listing);
+        await newListing.save();
+        req.flash("success", "New listing created!");
+        res.redirect("/listings")
+
+    }))
+//show route
+router.get("/:id", async (req, res) => {
+    let { id } = req.params;
+
+    let listing = await Listing.findById(id).populate("reviews")
+    if (!listing) {
+        req.flash("error", "Listing Does not exist!");
+        return res.redirect("/listings");
+    }
+    res.render("listings/show.ejs", { listing })
+})
+
+//form for update
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing Does not exist!");
+        return res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+}))
+//update after form redering
+router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    res.redirect(`/listings/${id}`);
+}))
+
+//delete route
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedList = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+}))
+
+
+
+
+module.exports = router;
